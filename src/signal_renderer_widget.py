36c22cff806e0f85d5fe9aa60bd5bc4792086dbf
Martin Vejmelka <vejmelkam@gmail.com>
2012-07-14 13:40:40 +0200

    added spectral display and moved labels to the right

--- src/signal_renderer_widget.py
+++ src/signal_renderer_widget.py
@@ -3,6 +3,8 @@ import numpy as np
 import bisect
 
 import pygame
+import scipy.signal
+
 
 from albow.widget import Widget, overridable_property
 from albow.theme import ThemeProperty
@@ -26,6 +28,7 @@ class SignalRendererWidget(Widget):
         self.cq_font = pygame.font.SysFont("Ubuntu", 16, True)
         self.multiplier = 1.0
         self.selected = range(14)
+        self.display_type = [0] * 14
 
 
     def select_channels(self, which):
@@ -39,14 +42,15 @@ class SignalRendererWidget(Widget):
         """
         Toggle the display of channel with index ndx (0..13).
         """
-#        print("Toggle chanbel %d" % ndx)
         if ndx in self.selected:
-            self.selected.remove(ndx)
+            if self.display_type[ndx] == 1:
+                self.selected.remove(ndx)
+            else:
+                self.display_type[ndx] = 1
         else:
             # need to re-sort the list after the append
             bisect.insort(self.selected, ndx)
-#            self.selected.append(ndx)
-#            self.selected.sort()
+            self.display_type[ndx] = 0
 
 
     def update_magnification(self, update):
@@ -75,9 +79,10 @@ class SignalRendererWidget(Widget):
         sig_amp = max(np.max(sig) - zero_lev, zero_lev - np.min(sig))
         if sig_amp == 0:
             sig_amp = 1.0
-        pixel_per_lsb = self.multiplier * frame.height / sig_amp / 2.0
-#        pixel_per_lsb = self.multiplier * frame.height / (50 / 0.51) / 2.0
+#        pixel_per_lsb = self.multiplier * frame.height / sig_amp / 2.0
+        pixel_per_lsb = self.multiplier * frame.height / (200.0 / 0.51)
         draw_pts_y = zero_ax_y - (sig - zero_lev) * pixel_per_lsb
+        draw_pts_y[draw_pts_y < frame.top] = frame.top
         draw_pts_x = np.linspace(0, frame.width, len(sig)) + frame.left
 
         pygame.draw.lines(surf, color, False, zip(draw_pts_x, draw_pts_y))
@@ -99,16 +104,29 @@ class SignalRendererWidget(Widget):
         """
         Render a spectral representation of the signal.
         """
+        min_freq = 0.7
+        max_freq = 45.0
+
+        s2 = sig.copy()
 
         # special check for all zeros (no data situation)
-        sig -= np.mean(sig)
-        if np.all(np.abs(sig) < 1.0):
-            sp = np.zeros(shape=(len(sig)/2,))
+        if np.all(s2 == 0.0):
+            sp = np.zeros(shape = (s2.shape[0] // 2, ))
         else:
-            sp = 20 * np.log(np.abs(np.fft.rfft(sig)))
+            tm = np.arange(len(sig), dtype = np.float64) / 128.0
+            angular_freqs = np.linspace(2.0 * np.pi * min_freq,
+                                        2.0 * np.pi * max_freq, 100)
+#            pg = scipy.signal.lombscargle(tm, s2, angular_freqs)
+#            sp = np.sqrt(4 * (pg / tm.shape[0]))
+            s2 = s2 - np.mean(s2)
+            sp = np.abs(np.fft.rfft(s2))
 
-        # autoscale the FFT display
-        sp -= np.amin(sp)
+        # if there are any non-finite values, replace buffer with zeros
+        if not np.all(np.isfinite(sp)):
+            sp[:] = 0.0
+
+        # autoscale the spectral display
+#        sp -= np.amin(sp)
         sig_amp = np.amax(sp)
         if sig_amp == 0:
             sig_amp = 1.0
@@ -122,6 +140,11 @@ class SignalRendererWidget(Widget):
 
         # draw the spectrum in dB
         pygame.draw.lines(surf, color, False, zip(draw_pts_x, draw_pts_y))
+
+        # draw spectral bands
+        for f in [5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0]:
+            x = (f - min_freq) / max_freq * frame.width + frame.left
+            pygame.draw.line(surf, (0, 0, 0), (x, frame.top), (x, frame.bottom))
 
         # fixme: draw 20dB? yardstick
 
@@ -143,8 +166,9 @@ class SignalRendererWidget(Widget):
             quality_color = (20, 150, 20)
 
         zero_ax_y = frame.top + frame.height // 2
-        surf.blit(self.font.render(chan_name, 1, (0,0,0)), (frame.left + 10, zero_ax_y - 10))
-        surf.blit(self.cq_font.render('%d (%s)' % (cq, cr_str), 1, quality_color), (frame.left + 10, zero_ax_y  + 10))
+        surf.blit(self.font.render(chan_name, 1, (0,0,0)), (frame.right - 150, zero_ax_y - 10))
+        surf.blit(self.cq_font.render('%d (%s)' % (cq, cr_str), 1, quality_color),
+                  (frame.right - 150, zero_ax_y  + 10))
 
 
     def draw(self, surf):
@@ -178,9 +202,10 @@ class SignalRendererWidget(Widget):
 
             # render a time series representation
             color = (255, 0, 0) if sndx % 2 == 0 else (0, 0, 255)
-            self.render_time_series(buf[:,s], color, rect, surf)
-#            self.render_time_series(buf[:,s], color, rect, surf)
-#            self.render_spectrum(buf[:,s], color, rect, surf)
+            if self.display_type[s] == 0:
+                self.render_time_series(buf[:,s], color, rect, surf)
+            else:
+                self.render_spectrum(buf[:,s], color, rect, surf)
 
             # draw the signal name
             self.render_name_and_contact_quality(chan_name, rect, surf)
